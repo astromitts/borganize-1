b65e08cd9cc0b34d37943696695a933de9f35387
Industry Dive <bomorin-id@MacBook-Pro.local>
2020-11-12 10:08:20 -0500

    Add heroku things

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python manage.py migrate --settings=app.settings_production; python manage.py collectstatic --noinput; gunicorn project.wsgi --log-file -
---
+++ app/settings_production.py
@@ -0,0 +1,19 @@
+import os
+from project.settings import *  # noqa
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DATABASE_USER'],
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': os.environ['DATABASE_HOST'],
+        'PORT': '5432'
+    }
+}
+
+ALLOWED_HOSTS = ['borganizer.herokuapp.com', 'herokuapp.com', ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
